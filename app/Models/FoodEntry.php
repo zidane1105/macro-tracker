8b473a8a703cb424911f638b806fb97e3557efd1
zidane1105 <hawarimifta@gmail.com>
2026-01-27 17:52:42 +0700

    Fix izin database

--- app/Models/FoodEntry.php
+++ app/Models/FoodEntry.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FoodEntry extends Model
 {
-    // Ini daftar kolom yang BOLEH diisi
-    protected $fillable = ['name', 'calories', 'protein', 'date'];
+    use HasFactory;
+
+    // INI KUNCINYA: Memberi izin kolom mana yang boleh diisi
+    protected $fillable = [
+        'name',
+        'calories',
+        'protein'
+    ];
 }
