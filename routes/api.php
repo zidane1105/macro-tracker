c9ee9225acea7ced0b22c4c5e9c020fa8ddc6405
zidane1105 <hawarimifta@gmail.com>
2026-01-27 18:56:19 +0700

    Tambah route post untuk simpan data

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,27 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\FoodEntryController;
+use App\Models\FoodEntry;
 
-// Ini jalur untuk mengambil data makanan
-Route::get('/foods', [FoodEntryController::class, 'index']);
+// 1. Jalur untuk MENGAMBIL data (GET)
+Route::get('/food-entries', function () {
+    return FoodEntry::latest()->get();
+});
 
-// Ini jalur untuk menyimpan makanan baru
-Route::post('/foods', [FoodEntryController::class, 'store']);
+// 2. Jalur untuk MENYIMPAN data (POST) - INI YANG KEMARIN KURANG
+Route::post('/food-entries', function (Request $request) {
+    // Validasi input (Biar aman)
+    $request->validate([
+        'name' => 'required',
+        'calories' => 'required|integer',
+        'protein' => 'nullable|integer'
+    ]);
 
-// Ini jalur untuk menghapus makanan
-Route::delete('/foods/{id}', [FoodEntryController::class, 'destroy']);
+    // Simpan ke database
+    return FoodEntry::create([
+        'name' => $request->name,
+        'calories' => $request->calories,
+        'protein' => $request->protein ?? 0 // Kalau kosong, isi 0
+    ]);
+});
